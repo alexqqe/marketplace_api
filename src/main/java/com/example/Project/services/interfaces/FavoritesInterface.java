95d83a862000568bd01defcf8cfac291e740e651
Александр Скоморохов <skomorohovaleksandr52@gmail.com>
2025-04-07 23:35:00 +0300

    add Favorites

--- src/main/java/com/example/Project/services/interfaces/FavoritesInterface.java
+++ src/main/java/com/example/Project/services/interfaces/FavoritesInterface.java
@@ -1,4 +1,16 @@
 package com.example.Project.services.interfaces;
 
+import com.example.Project.model.entity.Favorites;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.List;
+
 public interface FavoritesInterface {
+    void addFavorite(@NotNull Long userId, @NotNull Long productId);
+
+    List<Favorites> getFavoritesByUserId(@NotNull Long userId);
+
+    Favorites getFavoriteById(@NotNull Long id);
+
+    Favorites deleteFavoriteById(@NotNull Long id);
 }
